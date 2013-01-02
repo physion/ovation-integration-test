462faf362ddf07808bef323647d456d8f857b164
Barry Wark <barrywark@gmail.com>
2013-01-02 10:24:12 -0500

    DAO get(collection) in bulk
    Now passing first Project insertion test! Woot.

--- src/test/java/us/physion/ovation/integration/ProjectInsertionTest.java
+++ src/test/java/us/physion/ovation/integration/ProjectInsertionTest.java
@@ -19,19 +19,16 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(JukitoRunner.class)
-public class ProjectInsertionTest
-{
+public class ProjectInsertionTest {
 
     private static final String USER_NAME = "user";
     private static final String EMAIL = "email@email.com";
     private static final char[] PASSWORD = "password".toCharArray();
 
-    public static class Module extends JukitoModule
-    {
+    public static class Module extends JukitoModule {
 
         @Override
-        protected void configureTest()
-        {
+        protected void configureTest() {
             new OvationApiModule().configure(binder());
         }
     }
@@ -39,23 +36,20 @@ public class ProjectInsertionTest
     @Inject
     DataStoreCoordinator dsc;
 
-    @After
-    public void clean_up(CouchDbInstance server,
-                         CouchDbConnector db)
-    {
-        server.deleteDatabase(db.path());
-    }
-
     @Before
-    public void create_user()
-    {
+    public void create_user() {
         DataContext ctx = dsc.getContext();
         ctx.addUser(USER_NAME, EMAIL, PASSWORD);
     }
 
+    @After
+    public void clean_up(CouchDbInstance server,
+                         CouchDbConnector db) {
+        server.deleteDatabase(db.path());
+    }
+
     @Test
-    public void should_insert_project()
-    {
+    public void should_insert_project() {
         DataContext ctx = dsc.getContext();
         ctx.authenticateUser(USER_NAME, PASSWORD);
 
