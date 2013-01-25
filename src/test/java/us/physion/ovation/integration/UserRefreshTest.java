63f7ac1142667b5014ad18d489d4ea554d8647e6
Barry Wark <barrywark@gmail.com>
2013-01-24 22:45:20 -0500

    Fixed user refresh integraitno test

--- src/test/java/us/physion/ovation/integration/UserRefreshTest.java
+++ src/test/java/us/physion/ovation/integration/UserRefreshTest.java
@@ -13,38 +13,39 @@ import java.net.URI;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(JukitoRunner.class)
-public class UserRefreshTest extends IntegrationTestBase {
-    public static class Module extends JukitoModule {
+public class UserRefreshTest extends IntegrationTestBase
+{
+    public static class Module extends JukitoModule
+    {
 
         @Override
-        protected void configureTest() {
+        protected void configureTest()
+        {
             new OvationApiModule().configure(binder());
         }
     }
 
     @Test
-    public void should_refresh_password_from_separate_context() throws InterruptedException {
+    public void should_refresh_password_from_separate_context() throws InterruptedException
+    {
         DataContext ctx = dsc.getContext();
         ctx.authenticateUser(USER_NAME, PASSWORD);
 
-        final String username = "newuser";
-        final String email = "email@test.com";
-        final char[] password = "password".toCharArray();
-
-        User u = ctx.addUser(username, email, password);
+        User u = ctx.getAuthenticatedUser();
         URI userUri = u.getURI();
 
         DataContext ctx2 = dsc.getContext();
-        ctx2.authenticateUser(username, password);
-        assertEquals(u.getUuid(), ctx2.getAuthenticatedUser().getUuid());
+        ctx2.authenticateUser(USER_NAME, PASSWORD);
 
         User u2 = (User) ctx2.getObjectWithURI(userUri.toString());
         final String newEmail = "new@test.com";
         u2.updateEmail(newEmail);
-        assertEquals(newEmail, u2.getEmail());
 
         u.refresh();
 
+
+        assertEquals(newEmail, u2.getEmail());
+        assertEquals(u.getUuid(), ctx2.getAuthenticatedUser().getUuid());
         assertEquals(newEmail, u.getEmail());
     }
 }
